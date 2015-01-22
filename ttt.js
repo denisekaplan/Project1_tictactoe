6826e18ddd86351ae41101a2abb85a8eb5d46ca7
Denise Kaplan <denise.s.kaplan@gmail.com>
2015-01-22 11:45:02 -0800

    initial commit

---
+++ ttt.js
@@ -0,0 +1,8 @@
+var board = document.getElementById("board");
+for(var i = 0; i < 9; i++) {
+	var newDiv = document.createElement("div");
+	board.appendChild(newDiv);
+	newDiv.innerHTML = i;
+	newDiv.addEventListener("mouseenter", function().style.backgroundColor="blue";
+	newDiv.addEventListener("mouseleave").style.backgroundColor="blue";
+}
