79ab57af26e847734ed7ba75adf6c0cc6f7f0192
Denise Kaplan <denise.s.kaplan@gmail.com>
2015-01-23 11:45:57 -0800

    working on javaScript

--- ttt.js
+++ ttt.js
@@ -1,8 +1,31 @@
-var board = document.getElementById("board");
-for(var i = 0; i < 9; i++) {
-	var newDiv = document.createElement("div");
-	board.appendChild(newDiv);
-	newDiv.innerHTML = i;
-	newDiv.addEventListener("mouseenter", function().style.backgroundColor="blue";
-	newDiv.addEventListener("mouseleave").style.backgroundColor="blue";
-}
+// var x = "X"
+// var o = "O"
+// var count = 0;
+// var o_win = 0;
+// var x_win = 0;
+window.onload = function () {
+	confirm("Let's play!");
+	var xOro = prompt("You want to be X or O?");
+		if(xOro = "x") 
+		{
+			alert("Fine. Whatever. I wanted to be O anyway.");
+		}
+		else {
+			alert("Sweet! X is the best!");
+		}
+
+	var coinFlip = prompt("Flip the coin to see who goes first. \n Heads or Tails?");
+	
+	// coin flip
+	document.getElementById("quarter").onclick=function() {
+	    alert((Math.floor(Math.random() * 2) == 0) ? "Heads" : "Tails");
+	   };
+
+	   // if coinFlip = (Math.floor(Math.random() * 2) == 0) {
+	   // 	alert("You go first");
+	   // }
+	   // else	{
+	   // 	alert("Me first");
+	   // 	}
+
+}	
